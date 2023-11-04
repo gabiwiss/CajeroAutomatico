c9b57f37545403c6800139fa9da799d0a50ade79
gabi <gabi@DESKTOP-A5UB1DO>
2023-11-04 14:11:35 -0300

    Agregar archivos de proyecto.

---
+++ CajeroAutomaticoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CajeroAutomaticoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
