b97520013f7c47b977b1a37959cfc64c0215b72f
gabiwiss <gabrielwissler@gmail.com>
2023-11-06 00:01:31 -0300

    Agregado modelo Usuarios en web

---
+++ CajeroAutomaticoWeb/Models/UsuariosViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Web.Models
+{
+    public class UsuariosViewModel
+    {
+
+        public int Id { get; set; }
+        public string NroCuenta { get; set; }
+        public int Pin { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public bool Bloqueado { get; set; }
+
+    }
+}
